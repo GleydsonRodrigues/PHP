afc192ecc72099b194118698f17d716d362d8e29
Gleydson Rodrigues Matos da Silva <rgleydson794@gmail.com>
2021-03-30 19:56:16 -0300

    exercicio 3

---
+++ Exercicios/Ex3/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <Pre>
+
+<?php
+    include_once"metodos.php";
+
+    $p1 = new ContaBanco();
+    $p2 = new ContaBanco();
+
+    $p1->abrirConta("CC");
+    $p1->setDono("Jubileu");
+
+    $p2->abrirConta("CP");
+    $p2->setDono("Creuza");
+
+    $p2->deposito(1000);
+
+    $p2->sacar(500);
+    print_r($p1);
+    print_r($p2);
+
+
+?>    </Pre>
+</body>
+</html>
+
